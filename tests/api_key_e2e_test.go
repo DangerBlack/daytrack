31de2ec17b5018616f8fa9431f5fab5ebfe23c40
DangerBlack <daniele.baschieri@cubbit.io>
2024-04-20 16:15:56 +0200

    test(api_key): test list api keys

--- tests/api_key_e2e_test.go
+++ tests/api_key_e2e_test.go
@@ -29,3 +29,52 @@ func TestCreateApiKey(t *testing.T) {
 		return
 	}
 }
+
+func TestListApiKey(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var user *client.SignedUser
+	var apiKey, apiKey2 *models.ApiKey
+	var apiKeys *models.List[models.ApiKey]
+
+	if user, err = client.CreateUser(); err != nil {
+		t.Fatalf("unable to sign up %v", err)
+		return
+	}
+
+	if apiKey, err = client.CreateApiKey(user.Token, "test"); err != nil {
+		t.Fatalf("unable to create api key %v", err)
+		return
+	}
+
+	if apiKey2, err = client.CreateApiKey(user.Token, "test2"); err != nil {
+		t.Fatalf("unable to create api key %v", err)
+		return
+	}
+
+	if apiKeys, err = client.ListApiKeys(user.Token); err != nil {
+		t.Fatalf("unable to list api keys %v", err)
+		return
+	}
+
+	if len(apiKeys.Items) != 2 {
+		t.Fatalf("expected 2 api keys, got %d", len(apiKeys.Items))
+		return
+	}
+
+	for _, key := range apiKeys.Items {
+		if key.Key == "" {
+			t.Fatal("Failed unable to extract a valid api key")
+			return
+		}
+		if key.Name == "" {
+			t.Fatal("Failed unable to extract a valid api key name")
+			return
+		}
+		if key.Name != apiKey.Name && key.Name != apiKey2.Name {
+			t.Fatalf("expected key name to be test or test2, got %s", key.Name)
+			return
+		}
+	}
+}
--- tests/client/api_key_client.go
+++ tests/client/api_key_client.go
@@ -25,3 +25,21 @@ func CreateApiKey(token string, name string) (*models.ApiKey, error) {
 
 	return &response, nil
 }
+
+func ListApiKeys(token string) (*models.List[models.ApiKey], error) {
+	var err error
+	var response models.List[models.ApiKey]
+	url := BASE_URL + "/v1/api_keys"
+
+	if err = utils.DoRequest(
+		url,
+		utils.WithRequestMethod(http.MethodGet),
+		utils.WithExpectedStatusCode(http.StatusOK),
+		utils.WithAccessToken(token),
+		utils.ExtractGenericModel(&response),
+	); err != nil {
+		return nil, fmt.Errorf("failed unable to create get account request: %w", err)
+	}
+
+	return &response, nil
+}
