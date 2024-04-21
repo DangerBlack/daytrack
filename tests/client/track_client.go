c740f67b2a853cb60bdb16716359b8c69a40fa01
DangerBlack <daniele.baschieri@cubbit.io>
2024-04-21 18:58:57 +0200

    test(track): test public track and event

--- tests/client/track_client.go
+++ tests/client/track_client.go
@@ -27,6 +27,25 @@ func CreateTrack(token string, name string) error {
 	return nil
 }
 
+func UpdateTrack(token string, name string, visibility models.TrackVisibility) error {
+	var err error
+	url := BASE_URL + "/v1/tracks/" + name
+
+	if err = utils.DoRequest(
+		url,
+		utils.WithRequestMethod(http.MethodPatch),
+		utils.WithExpectedStatusCode(http.StatusOK),
+		utils.WithAccessToken(token),
+		utils.WithRequestBody(map[string]string{
+			"visibility": string(visibility),
+		}),
+	); err != nil {
+		return fmt.Errorf("failed unable to create track request: %w", err)
+	}
+
+	return nil
+}
+
 func ListTracks(token string) (*models.List[models.Track], error) {
 	var err error
 	var response models.List[models.Track]
--- tests/event_e2e_test.go
+++ tests/event_e2e_test.go
@@ -129,3 +129,91 @@ func TestListEventsByDay(t *testing.T) {
 		return
 	}
 }
+
+func TestListPublicEventsByDay(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var user, user2 *client.SignedUser
+	var apiKey, apiKey2 *models.ApiKey
+	var events *models.List[models.Day]
+	trackName := "water-plant"
+
+	if user, err = client.CreateUser(); err != nil {
+		t.Fatalf("unable to sign up %v", err)
+		return
+	}
+
+	if apiKey, err = client.CreateApiKey(user.Token, "test"); err != nil {
+		t.Fatalf("unable to create api key %v", err)
+		return
+	}
+
+	if user2, err = client.CreateUser(); err != nil {
+		t.Fatalf("unable to sign up %v", err)
+		return
+	}
+
+	if apiKey2, err = client.CreateApiKey(user2.Token, "test"); err != nil {
+		t.Fatalf("unable to create api key %v", err)
+		return
+	}
+
+	if err = client.CreateTrack(user.Token, trackName); err != nil {
+		t.Fatalf("unable to create api key %v", err)
+		return
+	}
+
+	if err = client.UpdateTrack(user.Token, trackName, models.TrackVisibilityPublicRead); err != nil {
+		t.Fatalf("unable to update track %v", err)
+		return
+	}
+
+	var tracks *models.List[models.Track]
+	if tracks, err = client.ListTracks(user.Token); err != nil {
+		t.Fatalf("unable to get track %v", err)
+		return
+	}
+
+	if len(tracks.Items) != 1 {
+		t.Fatalf("expected 1 track, got %d", len(tracks.Items))
+		return
+	}
+
+	if tracks.Items[0].Visibility != models.TrackVisibilityPublicRead {
+		t.Fatalf("expected track to be public read, got %s", tracks.Items[0].Visibility)
+		return
+	}
+
+	t1, _ := time.Parse(time.RFC3339, "2021-01-01T00:00:00Z")
+	t2, _ := time.Parse(time.RFC3339, "2021-01-01T00:00:00Z")
+	t3, _ := time.Parse(time.RFC3339, "2021-01-02T00:00:00Z")
+
+	for _, c := range []time.Time{t1, t2, t3} {
+		if err = client.TrackEvent(apiKey.Key, user.Username, trackName, 1, &c); err != nil {
+			t.Fatalf("unable to track event %v", err)
+			return
+		}
+	}
+
+	if events, err = client.ListEvents(apiKey2.Key, user.Username, trackName, nil); err != nil {
+		t.Fatalf("unable to list events %v", err)
+		return
+	}
+
+	if len(events.Items) != 2 {
+		t.Fatalf("expected 1 event, got %d", len(events.Items))
+		return
+	}
+
+	listBy := models.ListByRaw
+	if events, err = client.ListEvents(apiKey2.Key, user.Username, trackName, &listBy); err != nil {
+		t.Fatalf("unable to list events %v", err)
+		return
+	}
+
+	if len(events.Items) != 3 {
+		t.Fatalf("expected 1 event, got %d", len(events.Items))
+		return
+	}
+}
