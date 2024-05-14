2769956145048d3ac54c6fef236a5823200fd15c
DangerBlack <daniele.baschieri@cubbit.io>
2024-05-14 20:02:37 +0200

    feat(client): add login endpoint

---
+++ client/dailytrack/src/models/endpoints.js
@@ -0,0 +1,8 @@
+const YOUR_API_PORT = ":3000"
+
+const api_url = 'http://10.0.2.2:3000/v1';
+
+const user_url = `${api_url}/users`;
+const signin_url = `${api_url}/users/signin`;
+
+export { api_url, user_url, signin_url };
